073ad35b83922a514030f353f7341820f78fa273
Jay <yubing9421@qq.com>
2016-09-20 18:31:20 +0800

    修改包名称outfile--->outFile

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"dirtreeHash/dirTree"
-	"dirtreeHash/outfile"
+	"dirtreeHash/outFile"
 	"fmt"
 	"os"
 	"time"
@@ -12,26 +12,29 @@ func dowork() {
 	var path string
 	var a string
 	fmt.Println("输入目录的路径：")
-	fmt.Scanln(&path)
+	fmt.Scan(&path)
 	// 判断path是否有效， 若无效，则提醒用户重新输入path
 
 	fileList := dirTree.PrintFileDir(path)
 	//fileList := dirTree.PrintFileList(path)
-	outfile.GetfileName(fileList)
+	outFile.GetfileName(fileList)
 
 	//fmt.Println("文件路径为：", fileList)
-	fmt.Printf("输入quit退出，输入其他则继续：")
-	fmt.Scanln(&a)
+	fmt.Printf("输入quit退出，输入其他则继续：\t")
+	fmt.Scan(&a)
+	//fmt.scanln(&a)
 	switch a {
 	case "quit":
 		exit()
 	case "退出":
 		exit()
 	default:
+
 		dowork()
 	}
+
 	// 初始化变量
-	a = ""
+	//a = ""
 	fileList = make([]string, 0)
 }
 func exit() {
--- outFile/outfile.go
+++ outFile/outfile.go
@@ -1,4 +1,4 @@
-package outfile
+package outFile
 
 import (
 	"dirtreeHash/SHA1"
